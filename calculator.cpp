de1aeffc8cfbff1e9b5c823b87a7e0e6c72f574c
awszwc <xlb25@126.com>
2017-08-10 23:30:55 +1000

    give

--- calculator.cpp
+++ calculator.cpp
@@ -4,11 +4,11 @@
 #include <stack>
 #include <vector>
 
-void stream_process(int repeat_time,std::vector<std::string> stream, std::stack<std::string> &token_stack);
-
 void calculate_process(std::string s, std::stack<std::string> &token_stack);
 
 void check_stack(std::stack<std::string> &token_stack);
+
+bool check_number_valid(std::string &num);
 
 int main(int argc, char* argv[]) {
 
@@ -28,72 +28,36 @@ int main(int argc, char* argv[]) {
 
     // vector to store all of the input stream
     std::vector<std::string> input_stream;
+
+    std::vector<std::string> repeat_part;
+
+    int repeat_time;
+
+    bool repeat_flag = false;
     
     // read the file while we have input.
 	while (in >> s) {
-        input_stream.push_back(s);   // put the read string into vector
+        if (s == "repeat") {
+            check_stack(token_stack);
+            repeat_time = std::stoi(token_stack.top());
+            token_stack.pop();
+            repeat_flag = true;
+        } else if (s == "endrepeat") {
+            repeat_flag = false;
+            while (repeat_time-- > 0) {
+                for (auto i : repeat_part)
+                    calculate_process(i, token_stack);
+            }
+            repeat_part.clear();
+        } else {
+            if (repeat_flag)
+                repeat_part.push_back(s);
+            else
+                calculate_process(s, token_stack);
+        }
 	}
-    stream_process(1, input_stream, token_stack); // call function to do the process
 	in.close();
     return 0;
-}
-
-/* Function for input stream process, 3 parameters:
- *      integer 'reapt_time' : how many times need the stream be repeated
- *      vector 'stream' : may be whole input stream or part of stream that need to be repeated
- *      stack 'token_stack' : store the tokens for calculating
- * If meet 'repeat' command, create new stream vector to store the part that need be repeated,
- * then call itself to do recursion, otherwise call function 'calculate_process' to do some operations. */
-void stream_process(int repeat_time,std::vector<std::string> stream, std::stack<std::string> &token_stack) {
-    auto repeat_count = 0;   // use for matching the repeat with corresponding endrepeat
-    
-    int repeat_time_local = 0;  // store the repeated times if meet 'repeat' command in stream.
-    
-    bool repeat_flag = false;   // flag for check if already meet a 'repeat' command
-    
-    std::vector<std::string> stream_repeat_part;  // vector to store repeat content
-
-    // stream will be repeated until 'repeated_time' reach 0.
-    while (repeat_time-- > 0) {
-        // read the stream, store the string in 's'
-        for (auto s : stream) {
-            // if meet 'repeat' command
-            if (s == "repeat") {
-                // if 'repeat' occur more than once, count it and then push 'repeat' into new stream vector.
-                if (repeat_flag) {      
-                    ++repeat_count;
-                    stream_repeat_part.push_back(s);
-                } else {
-                    // if it is the first repeat in stream, get repeat time, clear the new stream vector, set a flag
-                    check_stack(token_stack);
-                    repeat_time_local = std::stoi(token_stack.top());
-                    token_stack.pop();
-                    repeat_flag = true;
-                    stream_repeat_part.clear();
-                }
-            } else if (s == "endrepeat") { // if meet 'endrepeat' command
-                // if this 'endrepeat' match the first 'repeat', call function stream_process to do recursion.
-                if (repeat_count == 0) {
-                    stream_process(repeat_time_local, stream_repeat_part, token_stack);
-                    repeat_flag = false;
-                } else {
-                    // if it is not match, minus repeat count by 1, then push 'endrepeat' into new stream vector.
-                    --repeat_count;
-                    stream_repeat_part.push_back(s);
-                }
-            }
-            else {
-                // if not 'repeat' or 'endrepeat' command
-                // if repeat flag is set, push token into new stream vector
-                // otherwise, call function 'calculate_process' to do some operation
-                if (repeat_flag)
-                    stream_repeat_part.push_back(s);
-                else
-                    calculate_process(s, token_stack);
-            }
-        }
-    }
-
 }
 
 /* Function for tokens excpet 'repeat' and 'endrepeat', 2 parameters
@@ -114,14 +78,13 @@ void calculate_process(std::string s, std::stack<std::string> &token_stack) {
     std::vector<std::string> reverse_vector;
 
     // detect if it is a number, if so, push it into stack
-    if (isdigit(s[0]) || (s[0] == '-' && isdigit(s[1])))
+    if (check_number_valid(s))
         token_stack.push(s);
     else {
-        // All command in this part need at least 1 number in stack, so check if stack is empty.
-        check_stack(token_stack);
         // handle the situation of 'add', 'sub', 'mult', 'div'
         if (s == "add" || s == "sub" || s == "mult" || s == "div") {
             // get two numbers from stack, store in 'x', 'y', before get second number, check if stack is empty.
+            check_stack(token_stack);
             x = token_stack.top();
             token_stack.pop();
             check_stack(token_stack);
@@ -147,7 +110,7 @@ void calculate_process(std::string s, std::stack<std::string> &token_stack) {
                 } else {
                     if (std::stod(y) == 0) { // divided by 0 will occur error.
                         std::cerr << std::endl << "Can not be divided by zero" << std::endl;
-                        exit(0);
+                        exit(EXIT_FAILURE);
                     }
                     std::cout << " / ";
                     z_double = std::stod(x) / std::stod(y);
@@ -174,7 +137,7 @@ void calculate_process(std::string s, std::stack<std::string> &token_stack) {
                 } else {
                     if (std::stoi(y) == 0) { // divided by 0 will occur error.
                         std::cerr << std::endl << "Can not be divided by zero" << std::endl;
-                        exit(0);
+                        exit(EXIT_FAILURE);
                     }
                     std::cout << " / ";
                     z_int = std::stoi(x) / std::stoi(y);
@@ -183,13 +146,14 @@ void calculate_process(std::string s, std::stack<std::string> &token_stack) {
                 token_stack.push(std::to_string(z_int));
             }
         } else if (s == "sqrt") {
+            check_stack(token_stack);
             // get number store in x, check if x is integer or double, calculate sqrt(x)
             // output calculation and store result back to stack
             x = token_stack.top();
             token_stack.pop();
             if (x.find('-') != std::string::npos) { // if number is negative, error will occur
                 std::cerr << "sqrt number is negative" << std::endl;
-                exit(0);
+                exit(EXIT_FAILURE);
             }
             if (x.find('.') != std::string::npos) { // if x is double, store result in 'z_double'
                 z_double = sqrt(std::stod(x));
@@ -201,15 +165,17 @@ void calculate_process(std::string s, std::stack<std::string> &token_stack) {
                 std::cout << "sqrt " << std::stoi(x) << " = " << z_int << std::endl;
                 token_stack.push(std::to_string(z_int));
             }
-        } else if (s == "pop") // pop the token
+        } else if (s == "pop") {// pop the token
+            check_stack(token_stack);
             token_stack.pop();
-        else if (s == "reverse") {
+        } else if (s == "reverse") {
+            check_stack(token_stack);
             // get the number and store in reverse_n, next reverse_n elements will be reversed in stack
             reverse_n = std::stoi(token_stack.top());
             token_stack.pop();
             if (reverse_n < 0) { // negative number will occur error
                 std::cerr << "reverse number is negative" << std::endl;
-                exit(0);
+                exit(EXIT_FAILURE);
             }
             // get next reverse_n elements, push back in vector 'reverse_vector' in order.
             while (reverse_n-- > 0) {
@@ -224,7 +190,7 @@ void calculate_process(std::string s, std::stack<std::string> &token_stack) {
         else {
             // the token is not a number or a command, error will occur
             std::cerr << "Invalid input " << s << std::endl;
-            exit(0);
+            exit(EXIT_FAILURE);
         }
     }
 }
@@ -233,6 +199,22 @@ void calculate_process(std::string s, std::stack<std::string> &token_stack) {
 void check_stack(std::stack<std::string> &token_stack) {
     if (token_stack.empty()) {
         std::cerr << "Stack is empty" << std::endl;
-        exit(0);
+        exit(EXIT_FAILURE);
     }
+}
+
+bool check_number_valid(std::string &num) {
+    bool point_flag = false;
+    if (isdigit(num[0]) || num[0] == '-') {
+        for (auto i = num.begin() + 1; i != num.end(); ++i) {
+            if (!isdigit(*i)) {
+                if (*i == '.' && point_flag == false)
+                    point_flag = true;
+                else
+                    return false;
+            }
+        }
+        return true;
+    } else
+        return false;
 }
